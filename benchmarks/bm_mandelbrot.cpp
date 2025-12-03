fd2767f07952947dfab576415eb5b83d3fa8498d
Stan Bergevoet <stanbergevoet@gmail.com>
2025-12-02 21:43:03 -0500

    Modularize benchmarks

--- benchmarks/bm_mandelbrot.cpp
+++ benchmarks/bm_mandelbrot.cpp
@@ -1,46 +1,41 @@
 #include <vector>
 
 #include "benchmark/benchmark.h"
-
 #include "mandelbrot_serial.hpp"
 #include "mandelbrot_omp.hpp"
 
+constexpr float REAL_MIN = -2.0f, REAL_MAX = 1.0f;
+constexpr float IMAG_MIN = -1.0f, IMAG_MAX =  1.0f;
 constexpr unsigned int MAX_ITER = 1000;
 
-static void BM_Mandelbrot_Serial(benchmark::State& state) {
+template <auto Func>
+inline void BM_Mandelbrot(benchmark::State& state) {
     const auto width  = state.range(0);
     const auto height = state.range(1);
 
     std::vector<uint8_t> output(width * height * 3);
 
     for (auto _ : state) {
-        mandelbrotSerial(output.data(), width, height, -2.0, 1.0, -1.0, 1.0, MAX_ITER);
+        Func(output.data(), width, height,
+             REAL_MIN, REAL_MAX,
+             IMAG_MIN, IMAG_MAX,
+             MAX_ITER);
     }
 }
 
-static void BM_Mandelbrot_OpenMP(benchmark::State& state) {
-    const auto width  = state.range(0);
-    const auto height = state.range(1);
+#define COMMON_ARGS \
+    ->Args({640, 480}) \
+    ->Args({1280, 720}) \
+    ->Args({1920, 1080}) \
+    ->Args({3840, 2160}) \
+    ->UseRealTime()
 
-    std::vector<uint8_t> output(width * height * 3);
+#define MANDEL_BENCH(NAME, FUNC) \
+    BENCHMARK(BM_Mandelbrot<FUNC>) \
+        ->Name(NAME) \
+        COMMON_ARGS;
 
-    for (auto _ : state) {
-        mandelbrotOMP(output.data(), width, height, -2.0, 1.0, -1.0, 1.0, MAX_ITER);
-    }
-}
-
-BENCHMARK(BM_Mandelbrot_Serial)
-    ->Args({640, 480})
-    ->Args({1280, 720})
-    ->Args({1920, 1080})
-    ->Args({3840, 2160})
-    ->UseRealTime();
-
-BENCHMARK(BM_Mandelbrot_OpenMP)
-    ->Args({640, 480})
-    ->Args({1280, 720})
-    ->Args({1920, 1080})
-    ->Args({3840, 2160})
-    ->UseRealTime();
+MANDEL_BENCH("Serial", mandelbrotSerial)
+MANDEL_BENCH("OMP",    mandelbrotOMP)
 
 BENCHMARK_MAIN();
